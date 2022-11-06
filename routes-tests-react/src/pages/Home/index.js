fac59490c6e8f9a72e7df31313176b4fd3f3a018
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2022-11-06 10:48:15 -0300

    link to product route added

--- routes-tests-react/src/pages/Home/index.js
+++ routes-tests-react/src/pages/Home/index.js
@@ -2,11 +2,17 @@ import { Link } from "react-router-dom"
 
 function Home() {
   return (
-    <div className="App">
-      <h1>Home</h1><br />
-      <Link to="/about">About</Link><br/>
-      <Link to="/contacs">Contacs</Link>
-    </div>
+    <>
+      <div className="App">
+        <h1>Home</h1><br />
+        <Link to="/about">About</Link><br />
+        <Link to="/contacs">Contacs</Link>
+
+        <hr />
+      <Link to="/product/524">PRODUCT 524</Link>
+      </div>
+    </>
+
   );
 }
 
